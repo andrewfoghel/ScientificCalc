4423bbfd2397190d4aedbe74004ec69cbc725c21
Andrew <you@example.com>
2018-02-26 11:47:09 -0700

    Initial Commit

---
+++ ScientificCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScientificCalc
+//
+//  Created by Andrew Foghel on 2/26/18.
+//  Copyright © 2018 andrewfoghel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
